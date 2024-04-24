42cf51325fd5f803b56252616e3022936d25c18a
Brian <76516934+brianwereh@users.noreply.github.com>
2024-04-24 23:49:54 +0300

    main commit

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+try :
+    with sr.Microphone() as source:
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
